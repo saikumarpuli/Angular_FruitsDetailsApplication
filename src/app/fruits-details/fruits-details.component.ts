fb123a1bcf2c42f208cadf89f0c93a1736c13a5c
saikumarpuli <saikumarpuli9@gmail.com>
2019-02-15 12:16:43 +0530

    "list and json"

--- src/app/fruits-details/fruits-details.component.ts
+++ src/app/fruits-details/fruits-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Fruits} from '../fruits';
 
 @Component({
   selector: 'app-fruits-details',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FruitsDetailsComponent implements OnInit {
 
+  @Input() fruity: Fruits;
   constructor() { }
 
   ngOnInit() {
