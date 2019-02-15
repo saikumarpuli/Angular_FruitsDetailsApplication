4a73c6a2f1108bb88aa9c33dcf2a9fa3359919ed
saikumarpuli <saikumarpuli9@gmail.com>
2019-02-15 10:47:02 +0530

    "list and json"

---
+++ src/app/fruits-deails/fruits-deails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fruits-deails',
+  templateUrl: './fruits-deails.component.html',
+  styleUrls: ['./fruits-deails.component.css']
+})
+export class FruitsDeailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
